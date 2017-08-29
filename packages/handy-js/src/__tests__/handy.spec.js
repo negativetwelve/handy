e27236193bf2128f3d71d774d9c54e0a57b4d0cc
Mark Miyashita <negativetwelve@gmail.com>
2017-08-29 05:23:51 -0700

    Export the Datetime class and create tests for exports

---
+++ packages/handy-js/src/__tests__/handy.spec.js
@@ -0,0 +1,11 @@
+// Modules
+import {Datetime} from '../handy';
+
+
+describe('Handy', () => {
+  describe('Datetime', () => {
+    it('should exist', () => {
+      expect(Datetime).toBeDefined();
+    });
+  });
+});
