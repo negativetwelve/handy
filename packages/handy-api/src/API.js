f8d872fcd9dba51f27311db943e0e9fed4133965
Mark Miyashita <negativetwelve@gmail.com>
2017-09-17 02:19:50 -0700

    Add ability to create a url, create the body, and request using the fetch polyfill

--- packages/handy-api/src/API.js
+++ packages/handy-api/src/API.js
@@ -1,11 +1,43 @@
+// Libraries
+import Url from 'handy-url';
+import fetch from 'isomorphic-fetch';
+
+
+/**
+ * API Client class that makes requests to a remote server.
+ */
 class API {
 
-  constructor({url}) {
-    this.url = url;
+  constructor({baseUrl}) {
+    this.baseUrl = baseUrl;
   }
 
-  async request({method, url, params, headers}) {
-    // TODO
+  createUrl({method = 'GET', path, params}) {
+    const url = `${this.baseUrl}${path}`;
+
+    switch (method) {
+      case 'GET':
+        return new Url(url, params);
+      case 'POST':
+        return new Url(url);
+    }
+  }
+
+  createBody({method, params, data}) {
+    if (method === 'GET') {
+      return;
+    } else if (data) {
+      return data;
+    } else if (params) {
+      return JSON.stringify(params);
+    }
+  }
+
+  async request({method, path, headers, params, data}) {
+    const url = this.createUrl({method, path, params}).toString();
+    const body = this.createBody({method, params, data});
+
+    return fetch(url, {method, headers, body});
   }
 
 }
